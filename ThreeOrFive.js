9ee682a2ae5b2603b1b0d3edeaaf04f2a38b2e92
gizemny <gizemny@gmail.com>
2015-09-21 18:18:23 -0500

    finished ThreeOrFive

--- ThreeOrFive.js
+++ ThreeOrFive.js
@@ -6,16 +6,17 @@ prompt.start()
 
 prompt.get(['number'], function (error, result) {
   var num = parseInt(result['number']);
-    if (num % 3 === 0) {
-    console.log(num + ' is divisible by 3!');
+  var divided; 
+	if (num % 3 === 0 && num % 5 === 0) {
+    divided = (num + ' is divisible by 3 & 5!')
+	} else if (num % 3 === 0) {
+    divided = (num + ' is divisible by 3!');
 	} else if (num % 5 === 0) {
-    console.log(num + ' is divisible by 5!');
-	  if (num % 3 === 0 && num % 5 === 0) {
-    console.log(num + ' is divisible by 3 & 5!')
-	} }
-	 else {
-	console.log(num + ' is not divisible by 3 or 5!');
+    divided = (num + ' is divisible by 5!');
+	} else {
+	divided = (num + ' is not divisible by 3 or 5!');
 	}
 
+console.log(divided);
 });
 

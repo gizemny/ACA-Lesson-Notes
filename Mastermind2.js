056a74087de03412112284e6acca92b148cc2408
gizemny <gizemny@gmail.com>
2015-10-06 21:58:02 -0500

    finished mastermind app

--- Mastermind2.js
+++ Mastermind2.js
@@ -5,72 +5,40 @@ prompt.start();
 var colors = require('colors/safe');
 
 // set up the board and define some global variables
-var board = [
-  [' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ']
-];
+var board = [];
 
 function printBoard() {
-  console.log(board[0].join(' '));
-  console.log(board[1].join(' '));
-  console.log(board[2].join(' '));
-  console.log(board[3].join(' '));
-  console.log(board[4].join(' '));
-  console.log(board[5].join(' '));
-  console.log(board[6].join(' '));
-  console.log(board[7].join(' '));
-  console.log(board[8].join(' '));
-  console.log(board[9].join(' '));
+  for (var i = 0; i < board.length; i ++) {
+    console.log(board[i]);
+  }
 }
 
-// var numTry = board.length
+var numTry = 0;
+var solution = '';
 
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
-
-function insertCode(pattern) {
-  var splitPattern = pattern.split('');
-  var message = computeMessage(splitPattern);
-//...
-board[numTry].push(message);
-
-  board[numTry][0] = splitPattern[0];
-  board[numTry][1] = splitPattern[1];
-  board[numTry][2] = splitPattern[2];
-  board[numTry][3] = splitPattern[3];
-}
-
-
-// generate random for solution
-var solution = 'ccdd';
-var guess = 'bdbb';
 
 function generateRandomCode() {
 	var letters = ['a', 'b', 'c', 'd', 'e' , 'g', 'h'];
-	solution = [
-    letters[getRandomInt(0, letters.length)],
-    letters[getRandomInt(0, letters.length)],
-    letters[getRandomInt(0, letters.length)],
-    letters[getRandomInt(0, letters.length)]
-].join('');
+	for (var i = 0; i < 4; i++) {
+    var randomIndex = getRandomInt(0, letters.length);
+    solution += letters[randomIndex];
+  }
 }
-// generate clues by comparing guess string to solution string
-function generateHints(solution, guess) {
-// first split the strings to arrays 
-	var guessArray = guess.split('');
-	var solutionArray = solution.split('');
 
-	// create a variable for correctPlacement
-	var correctPlacement = 0;
+function insertCode(guess) {
+  var guessArray = guess.split('');//  split the strings to arrays 
+  var message = generateHints(guess);
+  board.push(guess + ' ' + message); 
+}
+
+// generate clues by comparing guess string to solution string
+function generateHints(guess) {
+  var guessArray = guess.split('');//  split the strings to arrays 
+	var solutionArray = solution.split('');//  split the strings to arrays 
+	var correctPlacement = 0; // create a variable for correctPlacement
 	// create for loop that checks each items in the solution array against guess array
 	for (var i = 0; i < solutionArray.length; i++) {
 	// if there is a match, set the location to null
@@ -79,11 +47,10 @@ function generateHints(solution, guess) {
 			solutionArray[i] = null;
 		}
 	}
-
-	// create a variable for correctLetter
-	var correctLetter = 0;
+	var correctLetter = 0; // create a variable for correctLetter
 	// create for loop that checks indexOf each items in the guess array against solution array
 	// note: indexOf automatically returns -1 if there are no instances
+  // if there is a match it signifies correctLetter
 	for (var i = 0; i < solutionArray.length; i++	) {
 		var targetIndex = solutionArray.indexOf(guessArray[i]);
 		if (targetIndex > -1) {
@@ -91,45 +58,34 @@ function generateHints(solution, guess) {
 			solutionArray[targetIndex] = null;
 		}
 }
-
-// if there is a match it signifies correctLetter
-
-// repeat all of this while ... 
-// while () {
-
-// }
 return colors.red(correctPlacement + ' - ' + correctLetter);
 }
 
-// function checkWin() {
-// // if guessArray === solutionArray
-//  if (guessArray === solutionArray) {
-//  	return false;
-//  }
-// } 
-
+function checkWin(guess) {
+ if (guess === solution) {
+ 	console.log('You Win!')
+  return true;
+ } else {
+  return false;
+ }
+}
 
 function getPrompt() {
   printBoard();   
-  generateRandomCode();
   prompt.get(['guess'], function (error, result) {
-    console.log('input received: ' + result.guess);
-    // insertCode(result.guess);
-    generateHints(result.guess)
-    // checkSolution();
-      // if (checkSolution(result.pattern) === true) { 
-      //   return false;
-      // }   
+    // console.log('input received: ' + result['guess']);
+    if (checkWin(result['guess']) === true) {
+      return false;
+    };
+    insertCode(result['guess']);
+    generateHints(result['guess']);
     getPrompt();
-
   });
 }
+generateRandomCode();
+console.log(solution)
 
-
-
-console.log(generateHints(solution, guess));
-
-// getPrompt();
+getPrompt();
 
 
 
